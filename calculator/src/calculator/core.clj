27a021404f95c768c7419c7019481c081e4be011
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-08-23 09:41:50 +0100

    handle numbers separated by commas

--- calculator/src/calculator/core.clj
+++ calculator/src/calculator/core.clj
@@ -1,8 +1,14 @@
 (ns calculator.core
   (:gen-class))
+(require '[clojure.string :as str])
+
+
+(defn parse-numbers [numbers]
+  (str/split numbers #","))
+  
 
 (defn add [numbers]
   (if (empty? numbers) 0
-    (Integer/parseInt numbers)))
+    (reduce + (map #(Integer/parseInt %) (parse-numbers numbers)))))
 
 
--- calculator/test/calculator/core_test.clj
+++ calculator/test/calculator/core_test.clj
@@ -7,3 +7,6 @@
 
 (deftest adding-a-string-number-returns-number
     (is (= 1 (add "1"))))
+
+(deftest adding-2-string-numbers-separated-by-comma-returns-sum-of-numbers
+    (is (= 3 (add "1,2"))))
