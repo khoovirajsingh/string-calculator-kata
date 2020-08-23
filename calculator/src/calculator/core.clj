d2f2dc841fbf52260821bf33c4bffc7f89be7ea6
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-08-23 10:21:11 +0100

    handling newline separator

--- calculator/src/calculator/core.clj
+++ calculator/src/calculator/core.clj
@@ -2,11 +2,17 @@
   (:gen-class))
 (require '[clojure.string :as str])
 
-
 (defn parse-numbers [numbers]
   (mapcat #(str/split % #"\n") (str/split numbers #",")))
-  
 
+(defn custom-separators? [input]
+  (= '("/" "/") (take 2 (str/split input #""))))
+
+(defn parse-separators [input]
+  (let [dropped (drop 2 (str/split input #""))
+        separator (first dropped)
+        numbers (drop 1 (rest dropped))]
+    {:separators separator :numbers (str/join numbers)}))
 
 (defn add [numbers]
   (if (empty? numbers) 0
@@ -14,6 +20,5 @@
          (parse-numbers)
          (map #(Integer/parseInt %))
          (reduce +))))
-
 
 
--- calculator/test/calculator/core_test.clj
+++ calculator/test/calculator/core_test.clj
@@ -8,4 +8,11 @@
      1 (add "1")
      3 (add "1,2")
     45 (add "1,2,3,4,5,6,7,8,9")
-     6 (add "1\n2,3")))
+     6 (add "1\n2,3")
+     ))
+
+(deftest check-for-custom-separators
+  (is (= true (custom-separators? "//;\n1;2"))))
+
+(deftest parse-custom-separators
+  (is (= {:separators ";" :numbers "1;2"} (parse-separators "//;\n1;2"))))
